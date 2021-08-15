cd88b72dbb50f47c070b476e6a88d1e6e89b5808
bharatmahaur <75228076+bharatmahaur@users.noreply.github.com>
2021-08-15 06:08:17 +0530

    Delete ssd/models directory

--- ssd/models/custom_layers/__init__.py
+++
@@ -1,8 +0,0 @@
-##
-## /src/models/custom_layers/__init__.py
-##
-## Created by Paul Warkentin <paul@warkentin.email> on 15/07/2018.
-## Updated by Paul Warkentin <paul@warkentin.email> on 15/07/2018.
-##
-
-from .layers import *
